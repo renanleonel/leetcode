1f86a4db94cc70707086b2b184bbf918336db70d
renanleonel <renanleonelpro@gmail.com>
2024-04-11 20:37:14 -0300

    2327

---
+++ 2327.py
@@ -0,0 +1,22 @@
+nums = [0,1,4,6,7,10]
+diff = 3
+
+def arithmeticTriplets(nums, diff):
+    hashmap = {}
+    ans = 0
+
+    for i in range(len(nums)):
+        if nums[i] - diff in hashmap and nums[i] - 2*diff in hashmap:
+            ans +=1
+        
+        hashmap[nums[i]] = 1
+    
+    return ans
+
+
+
+
+                
+
+
+print(arithmeticTriplets(nums, diff))
