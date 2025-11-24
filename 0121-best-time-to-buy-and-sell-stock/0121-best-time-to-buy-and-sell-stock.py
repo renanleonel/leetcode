414b0535fd4a6dc4b236c1e6febd2bc9f0138de3
Renan Leonel <62075475+renanleonel@users.noreply.github.com>
2025-11-23 23:00:06 -0300

    Time: 125 ms (30.86%), Space: 26.9 MB (77.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,14 +3,12 @@ class Solution:
         l, r = 0, 1
         _max = 0
 
-        while l < r and r <= len(prices) - 1:
+        while r < len(prices):
             diff = prices[r] - prices[l]
             _max = max(_max, diff)
 
             if prices[r] < prices[l]:
                 l = r
-                r = l + 1
-            else:
-                r += 1
+            r += 1
 
         return _max
