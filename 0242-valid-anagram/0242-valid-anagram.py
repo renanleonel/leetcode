e7f150d01ede316134d2d9d16ba54f84678e79e7
Renan Leonel <62075475+renanleonel@users.noreply.github.com>
2026-01-03 12:10:01 -0300

    Time: 11 ms (74.16%), Space: 17.6 MB (84.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,7 +5,7 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        for i in range(len(s) - 1):
+        for i in range(len(s)):
             hashS[s[i]] = hashS.get(s[i], 0) + 1
             hashT[t[i]] = hashT.get(t[i], 0) + 1
 
